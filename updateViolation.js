88b8971d4be75d7a5e681d1c29b73003daa42cca
GG-Yeh <b113040026@g-mail.nsysu.edu.tw>
2024-12-16 06:38:17 +0800

    Update updateViolation.js

--- updateViolation.js
+++ updateViolation.js
@@ -116,7 +116,7 @@ document.addEventListener('DOMContentLoaded', () => {
             headers: {
                 'Content-Type': 'application/json',
             },
-            body: JSON.stringify(updatedUserData),
+            body: JSON.stringify(violationData),
         })
             .then(response => {
                 if (!response.ok) {
